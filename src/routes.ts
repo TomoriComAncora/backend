10fcb9383ae200100e76772c05e18faec6050bdd
TomoriComAncora <lucas_fernando@ufms.br>
2024-09-17 16:07:00 -0400

    Feat: Criando token de usuario

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/user/createUserController"
+import { AuthUserController } from "./controllers/user/authUserController"
 
 const router = Router();
 
 router.post("/users", new CreateUserController().handle);
+
+router.post("/session", new AuthUserController().handle);
 
 export { router };
