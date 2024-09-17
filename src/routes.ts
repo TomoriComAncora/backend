6d6292636d8a8ea62fa4dd6e2e3648b402ae2f23
TomoriComAncora <lucas_fernando@ufms.br>
2024-09-17 16:40:55 -0400

    Feat: Começando o middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { CreateUserController } from "./controllers/user/createUserController"
-import { AuthUserController } from "./controllers/user/authUserController"
+import { CreateUserController } from "./controllers/user/createUserController";
+import { AuthUserController } from "./controllers/user/authUserController";
+import { DetailUserController } from "./controllers/user/detailUserController";
+
+import { authenticated } from "./middlewares/authenticated";
 
 const router = Router();
 
 router.post("/users", new CreateUserController().handle);
 
 router.post("/session", new AuthUserController().handle);
+
+router.get("/me", authenticated, new DetailUserController().handle);
 
 export { router };
