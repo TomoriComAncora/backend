259859328c33bd5eb50613c847f1f0beb3ce0fcf
TomoriComAncora <lucas_fernando@ufms.br>
2024-09-19 16:45:33 -0400

    Feat: Verificando token

--- src/middlewares/authenticated.ts
+++ src/middlewares/authenticated.ts
@@ -1,7 +1,22 @@
 import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface Payload{
+    sub: string;
+}
 
 export function authenticated(req: Request, res: Response, next: NextFunction) {
-  console.log("Passou pelo middleware");
 
-  return next();
+    //Recebendo o token
+    const AuthToken = req.headers.authorization;
+
+    //Verificando se o token foi enviado
+    if(!AuthToken){
+        return res.status(401).end();
+    }
+
+    //Separando as strings do token
+    const [, token] = AuthToken.split(" ");
+
+    console.log(token);
 }
