ce9d25ab5accecaa94a5cc95da9d9e58485f1fd8
TomoriComAncora <lucas_fernando@ufms.br>
2024-09-24 16:53:32 -0400

    Feat: Criando categorias

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/user/createUserController";
 import { AuthUserController } from "./controllers/user/authUserController";
 import { DetailUserController } from "./controllers/user/detailUserController";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController"
 
 import { authenticated } from "./middlewares/authenticated";
 
@@ -12,5 +13,8 @@ router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 
 router.get("/me", authenticated, new DetailUserController().handle);
+
+//routas de categoria
+router.post("/category", authenticated, new CreateCategoryController().handle)
 
 export { router };
