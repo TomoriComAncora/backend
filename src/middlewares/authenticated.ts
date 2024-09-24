966a6b9f2b01df5e1bb8eafaf0f9f5979ed60700
TomoriComAncora <lucas_fernando@ufms.br>
2024-09-24 16:52:10 -0400

    Feat: Middleware e detalhes do usuario

--- src/middlewares/authenticated.ts
+++ src/middlewares/authenticated.ts
@@ -1,22 +1,30 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-interface Payload{
-    sub: string;
+interface Payload {
+  sub: string;
 }
 
 export function authenticated(req: Request, res: Response, next: NextFunction) {
+  //Recebendo o token
+  const AuthToken = req.headers.authorization;
 
-    //Recebendo o token
-    const AuthToken = req.headers.authorization;
+  //Verificando se o token foi enviado
+  if (!AuthToken) {
+    return res.status(401).end();
+  }
 
-    //Verificando se o token foi enviado
-    if(!AuthToken){
-        return res.status(401).end();
-    }
+  //Separando as strings do token
+  const [, token] = AuthToken.split(" ");
 
-    //Separando as strings do token
-    const [, token] = AuthToken.split(" ");
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET) as Payload;
 
-    console.log(token);
+    //recuperar o id do token e colocando dentro da variavel user_id dentro do request
+    req.user_id = sub;
+
+    return next();
+  } catch (err) {
+    return res.status(401).end();
+  }
 }
