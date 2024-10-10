d4c3a3f7f50b2d427f62047dfe75d1941163f834
TomoriComAncora <lucas_fernando@ufms.br>
2024-10-10 07:58:54 -0400

    Feat: Criando um produto e salvando imagem

---
+++ src/controllers/product/createProductController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { CreateProductService } from "../../services/product/createProductService";
+
+class CreateProductController {
+  async handle(req: Request, res: Response) {
+    const { name, price, description, category_id } = req.body;
+
+    const createProductController = new CreateProductService();
+
+    if (!req.file) {
+      throw new Error("A foto do produto precisa ser enviada!");
+    } else {
+      const { originalname, filename } = req.file;
+      console.log(filename);
+
+      const product = await createProductController.execute({
+        name,
+        price,
+        description,
+        category_id,
+        banner: "",
+      });
+
+      return res.json(product);
+    }
+  }
+}
+
+export { CreateProductController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
+import multer from "multer";
 import { CreateUserController } from "./controllers/user/createUserController";
 import { AuthUserController } from "./controllers/user/authUserController";
 import { DetailUserController } from "./controllers/user/detailUserController";
-import { CreateCategoryController } from "./controllers/category/CreateCategoryController"
-import { ListCategoryController } from "./controllers/category/ListCategoryController"
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/createProductController";
 
 import { authenticated } from "./middlewares/authenticated";
+import uploadconfig from "./config/multer";
 
 const router = Router();
+const upload = multer(uploadconfig.upload("./tmp"));
 
 router.post("/users", new CreateUserController().handle);
 
@@ -15,9 +19,17 @@ router.post("/session", new AuthUserController().handle);
 
 router.get("/me", authenticated, new DetailUserController().handle);
 
-//routas de categoria
-router.post("/category", authenticated, new CreateCategoryController().handle)
+//rotas de categoria
+router.post("/category", authenticated, new CreateCategoryController().handle);
 
 router.get("/category", authenticated, new ListCategoryController().handle);
+
+//rotas produtos
+router.post(
+  "/product",
+  authenticated,
+  upload.single("file"),
+  new CreateProductController().handle
+);
 
 export { router };
