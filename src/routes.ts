1b77b24d89a585a9483d3c86d05147ce597590b8
TomoriComAncora <lucas_fernando@ufms.br>
2024-11-06 16:22:38 -0400

    Feat: Listar itens de cada pedido

--- src/routes.ts
+++ src/routes.ts
@@ -16,6 +16,7 @@ import { AddItemController } from "./controllers/order/addItemController";
 import { RemoveItensController } from "./controllers/order/removeItensController";
 import { SendOrderController } from "./controllers/order/sendOrderController";
 import { ListOrdersController } from "./controllers/order/listOrdersController";
+import { DetailsOrderController } from "./controllers/order/detailsOrderController";
 
 import { authenticated } from "./middlewares/authenticated";
 import uploadconfig from "./config/multer";
@@ -23,30 +24,22 @@ import uploadconfig from "./config/multer";
 const router = Router();
 const upload = multer(uploadconfig.upload("./tmp"));
 
+//criar usuario
 router.post("/users", new CreateUserController().handle);
 
+//logar com usuario
 router.post("/session", new AuthUserController().handle);
 
+//detalhes do usuario
 router.get("/me", authenticated, new DetailUserController().handle);
 
 //rotas de categoria
 router.post("/category", authenticated, new CreateCategoryController().handle);
-
 router.get("/category", authenticated, new ListCategoryController().handle);
 
 //rotas produtos
-router.post(
-  "/product",
-  authenticated,
-  upload.single("file"),
-  new CreateProductController().handle
-);
-
-router.get(
-  "/category/product",
-  authenticated,
-  new ListByCategoryController().handle
-);
+router.post("/product", authenticated, upload.single("file"), new CreateProductController().handle);
+router.get("/category/product", authenticated, new ListByCategoryController().handle);
 
 //rotas de pedidos
 router.post("/order", authenticated, new CreateOrderController().handle);
@@ -55,5 +48,6 @@ router.post("/order/add", authenticated, new AddItemController().handle);
 router.delete("/order/remove", authenticated, new RemoveItensController().handle);
 router.put("/order/send", authenticated, new SendOrderController().handle);
 router.get("/orders", authenticated, new ListOrdersController().handle);
+router.get("/orders/details", authenticated, new DetailsOrderController().handle);
 
 export { router };
